df05d642fdfc4190039b2acc16be248e0b95ddb1
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-09 13:55:12 -0300

    tipos de retornos metodos AdicionarAluno e ObterQuantidadeDeAlunosMatriculados

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -15,5 +15,11 @@ namespace ExemploExplorando.Models
         {
             Alunos.Add(aluno);
         }
+
+        public int ObterQuantidadeDeAlunosMatriculados()
+        {
+            int quantidade = Alunos.Count;
+            return quantidade;
+        }
     }
 }

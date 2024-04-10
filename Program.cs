fa0f556847b7d41ce72819cd42e7583cbb74df6e
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-10 11:03:07 -0300

    Concatenacao de valores

--- Program.cs
+++ Program.cs
@@ -1,19 +1,45 @@
 ﻿using ExemploExplorando.Models;
 
 
-Pessoa p1 = new Pessoa(nome:"Wellington",sobrenome:"Feijó");
+string numero1 = "10";
+string numero2 = "20";
+
+string resultado = numero1 + numero2;
+
+Console.WriteLine(resultado);
 
 
-Pessoa p2 = new Pessoa(nome:"Eduardo",sobrenome:"Queiroz");
 
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Ingles";
-cursoDeIngles.Alunos = new List<Pessoa>();
 
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa(nome:"Wellington",sobrenome:"Feijó");
+
+
+// Pessoa p2 = new Pessoa(nome:"Eduardo",sobrenome:"Queiroz");
+
+
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Ingles";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.ListarAlunos();
 
 
 

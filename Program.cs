7fedecea3a896658d218713d2e10888e0e75a238
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-10 11:27:18 -0300

    Representando porcentagem e valor personalizado

--- Program.cs
+++ Program.cs
@@ -6,8 +6,13 @@ CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
 
 decimal valorMonetario = 1582.40M;
 
-Console.WriteLine(valorMonetario.ToString("C8"));
+Console.WriteLine(valorMonetario.ToString("C"));
 
+double porcentagem = .3421;
+Console.WriteLine(porcentagem.ToString("P"));
+
+int numero =123456;
+Console.WriteLine(numero.ToString("##-##-##"));
 
 
 

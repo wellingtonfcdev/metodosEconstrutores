c376540aeba752a1391d6b20311a40e05f84ae3f
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-10 16:12:32 -0300

    Adicionando e removendo dados da fila com Enqueue e Dequeue

--- Program.cs
+++ Program.cs
@@ -1,8 +1,34 @@
 ﻿using ExemploExplorando.Models;
 using System.Globalization;
 
+Queue<int> fila = new Queue<int>();
 
-new ExemploExcecao().Metodo1();
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+
+
+foreach(int item in fila)
+{
+    Console.WriteLine(item);
+}
+
+//remover da fila
+Console.WriteLine($"Removendo o elemento {fila.Dequeue()}") ;
+fila.Enqueue(10);
+foreach(int item in fila)
+{
+    Console.WriteLine(item);
+}
+
+
+
+
+
+
+
+//new ExemploExcecao().Metodo1();
 
 
 

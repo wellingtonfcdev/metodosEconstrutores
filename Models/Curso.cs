dad2bb6a96d0f0d4e2b96a87d612a1c16e4e5a69
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-10 10:14:16 -0300

    Concatenacao de strings

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -31,10 +31,13 @@ namespace ExemploExplorando.Models
         public void ListarAlunos()
     {
         Console.WriteLine($"Alunos do curso de {Nome}");
-        foreach (Pessoa aluno in Alunos)
+
+        for(int count = 0; count < Alunos.Count; count++)
         {
-            Console.WriteLine(aluno.NomeCompleto);
+            string texto = "Nº " + count + " - " + Alunos[count].NomeCompleto;
+            Console.WriteLine(texto);
         }
+
     }
     }
 
--- obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34debe8390e2a709497023bd22ac83c956f15ff4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+826f1a4d7fec61d10ebefbd382c61fa8ee3abafa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

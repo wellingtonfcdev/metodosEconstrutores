ac49ffa3cc7b502b343cf05d66197977a01a69ab
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-11 11:09:32 -0300

    Outras formas de criar uma Tupla

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Globalization;
 //Tupla
 
 (int, string, string, decimal) tupla = (1,"Wellington","Feijó", 1.80M);
+ValueTuple<int, string, string, decimal>outroExemploTupla = (1,"Wellington","Feijó",1.80M);
+var outroExemploTuplaCreate = Tuple.Create(1,"Wellington","Feijó",1.80M);
 
 Console.WriteLine($"Id: {tupla.Item1}");
 Console.WriteLine($"Nome: {tupla.Item2}");

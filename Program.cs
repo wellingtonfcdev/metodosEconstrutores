c030f6c2edcc027889177cb9e3dd213ac4f640c9
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-11 15:38:02 -0300

    Escrevendo arquivo no formato json

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ Venda v1 = new Venda(1,"Material de escritório", 25.00M);
 
 string serializado = JsonConvert.SerializeObject(v1,Formatting.Indented);
 
+File.WriteAllText("Arquivos/venda.json", serializado);
 Console.WriteLine(serializado);
 
 
